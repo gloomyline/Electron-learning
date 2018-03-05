298440953057d4d3f58995c550dce49018c1454e
1211071880@qq.com <1211071880@qq.com>
2018-03-05 15:23:51 +0800

    Init: component Topbar.

---
+++ demos/space-snake/src/renderer/components/Topbar/Topbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="topbar">
+    This is demo vue component - topbar.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topbar',
+  data () {
+    return {}
+  },
+  mounted () {},
+  computed: {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="stylus" ref="stylesheet/stylus" scoped>
+</style>
